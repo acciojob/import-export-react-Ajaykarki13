419455b6550532aa5c40f34f4b7937b178abfeda
Ajaykarki13 <ajaymum698@gmail.com>
2023-03-22 20:00:07 +0530

    soln

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,12 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <Heading />
+      <InputQuery />
+      <SubHeading />
+      <SubmitButton />
     </div>
   )
 }
 
-
+export default App
